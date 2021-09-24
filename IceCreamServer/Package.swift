fed34341bd810389ca8fbc110507e74c171fc2dd
Stephen Kockentiedt <stephen@kockentiedt.name>
2021-09-24 22:01:03 +0200

    Update to version 0.2.0 of SwiftyBridges

--- IceCreamServer/Package.swift
+++ IceCreamServer/Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
         .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0"),
         .package(url: "https://github.com/vapor/fluent-sqlite-driver.git", from: "4.0.0"),
-        .package(url: "https://github.com/SwiftyBridges/SwiftyBridgesVapor.git", .upToNextMinor(from: "0.1.2")),
+        .package(url: "https://github.com/SwiftyBridges/SwiftyBridgesVapor.git", .upToNextMinor(from: "0.2.0")),
     ],
     targets: [
         .target(
